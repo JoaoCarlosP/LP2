53b1530103904ca017347f182c4285d90b6d9804
JoaoCarlosP <Joaocarlosjoe74@gmail.com>
2022-05-13 19:00:52 -0300

    Concluindo o exercicio 4 da Atividade 9

--- Atividade9/PAtividade9/PAtividade9/FrmExercicio4.cs
+++ Atividade9/PAtividade9/PAtividade9/FrmExercicio4.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace PAtividade9
 {
@@ -15,6 +16,30 @@ namespace PAtividade9
         public FrmExercicio4()
         {
             InitializeComponent();
+        }
+
+        private void btnClique_Click(object sender, EventArgs e)
+        {
+            string auxiliar = "";
+
+            List<string> listaNomes = new List<string>(new string[] {"Ana", "André", "Débora", "Fátima", "João",
+                    "Janete", "Otávio", "Marcelo","Pedro", "Thais"});
+
+            listaNomes.Remove("Otávio");
+
+            foreach(string a in listaNomes)
+            {
+                auxiliar = auxiliar + a + "\n";
+            }
+
+            MessageBox.Show(auxiliar);
+
+
+
+
+
+
+
         }
     }
 }
